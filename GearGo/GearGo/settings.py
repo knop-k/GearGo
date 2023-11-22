43cac1d0d84bc36c80e6b440a228e83048c29c46
knop-k <knop.krzysiek@gmail.com>
2023-11-22 16:56:33 +0100

    refactor: add templates to dirs in settings.py

--- GearGo/GearGo/settings.py
+++ GearGo/GearGo/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from decouple import config
 import platform
 import json
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'GearGo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
