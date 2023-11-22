bf794fb6fc23f13381daa6d2012041bd6c843c8c
knop-k <knop.krzysiek@gmail.com>
2023-11-22 15:38:20 +0100

    feat: create Custom User model

--- GearGo/apps/users/models.py
+++ GearGo/apps/users/models.py
@@ -1,1 +1,8 @@
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+    pass
+
+    def __str__(self):
+        return self.username
